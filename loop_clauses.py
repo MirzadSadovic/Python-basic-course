bdac06a5199088844ce93a6851a2acbdb7c6e598
Mirzad Sadovic <mirzadsadovic@gmail.com>
2022-11-27 08:21:48 +0100

    first commit

---
+++ loop_clauses.py
@@ -0,0 +1,51 @@
+#Python control flows - Loop clauses
+
+'''
+Python has a few statements and clauses that we can use in loops. 
+These are:
+break
+continue
+else 
+pass
+
+Loop statements may have an else clause; it is executed when the 
+loop terminates through exhaustion of the iterable (with for) or 
+when the condition becomes false (with while), but not when the 
+loop is terminated by a break statement. 
+'''
+
+#Break statement
+for n in range(2, 10): #equivalent of...for n in [2,3,4,5,6,7,8,9]:
+    for x in range(2, n): #first loop is for x in range(2, 2):
+        if n % x == 0: 
+            print(n, 'equals', x, '*', n//x)
+            break
+    #the else runs when no break clause occurs
+    else:
+        print(n, 'is a prime number')
+
+    #2 is a prime number 
+    #3 is a prime number
+    #4 equals 2 * 2
+    #5 is a prime number
+    #6 equals 2 * 3
+    #7 is a prime number
+    #8 equals 2 * 4
+    #9 equals 3 * 3 -> ???
+
+
+#Continue statement
+for num in range(2, 10): #equivalent of...for n in [2,3,4,5,6,7,8,9]:
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue #Will continue with the next loop
+    print("Found an odd number", num)
+
+
+#Pass statement
+class MyPassClass:
+    pass
+
+
+def my_pass_def(*args):
+    pass #Needs looking at
